b8d69c57302c00b722ce7639727ff9f5f30418ff
Fabio Frattarelli <fabiofrt@outlook.it>
2022-12-16 10:00:02 +0100

    esercizio completo

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class Driver {
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Driver {
+    public static void main(String[] args) throws SQLException {
+
+        Connection connection = null;
+
+        try {
+
+            String url = "jdbc:mysql://localhost:3306/newdb";
+            String user = "developer";
+            String password = "developer";
+
+            connection = DriverManager.getConnection(url, user, password);
+
+            Statement statement = connection.createStatement();
+
+            ResultSet query = statement.executeQuery("select * from students");
+
+            List<String> surnames = new ArrayList<>();
+
+            System.out.println("_________ Name _________");
+
+            while(query.next()){
+                System.out.println(query.getString(3));
+                surnames.add(query.getString(2));
+            }
+
+            System.out.println("________ Surname ________");
+
+            surnames.stream().forEach(System.out::println);
+
+        } catch (SQLException e){
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (connection != null)
+                    connection.close();
+            } catch(SQLException ex){
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
 }
